34c68e30461717beb9042ef120f1482f46e5b04f
Cate <catek.dev@gmail.com>
2026-03-17 05:32:44 +1100

    Updated the base in both the router and Vite config

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router basename="/aeden-labs">
+    <Router basename="/">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/privacy-policy" element={<Privacy />} />
